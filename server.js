be14cb89efabc58f20fe3eae94c341be86b5c7ab
ihor <krylro@gmail.com>
2018-08-27 14:01:29 +0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var app = require('./app');
 
 var port = process.env.PORT || 3000; 	  // set our port
 var host = process.env.HOST || '0.0.0.0'; // For Heroku to run successfully
-
-http.createServer(app).listen(port, host, () => {
+///var/cpanel/ssl/cpanel/mycpanel.pem
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+},app).listen(port, host, () => {
 	console.log("Server ready at http://" + host + ":" + port);
 });
