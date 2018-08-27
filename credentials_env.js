bc54d2d202df158ae560b3efe643175f88384b5c
ihor <krylro@gmail.com>
2018-08-27 11:36:37 +0800

    update

--- credentials_env.js
+++ credentials_env.js
@@ -1,7 +1,7 @@
  module.exports = {
      host     : '127.0.0.1',
-     user     : 'root',
-     password : 'root',
+     user     : 'targethit',
+     password : 'poil131313',
      database : 'targethit_wordpress',
      port: 3306
 }
