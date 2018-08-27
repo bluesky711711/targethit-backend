10991e1b87220339fb65ae120a179216e0b3a077
ihor <krylro@gmail.com>
2018-08-27 19:50:23 +0800

    update

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var port = process.env.PORT || 3000; 	  // set our port
 var host = process.env.HOST || '0.0.0.0'; // For Heroku to run successfully
 
 ///var/cpanel/ssl/cpanel/mycpanel.pem
-// https.createServer({
-// 	key: fs.readFileSync('server.key'),
-// 	cert: fs.readFileSync('server.cert')
-// },app).listen(port, host, () => {
-// 	console.log("Server ready at https://" + host + ":" + port);
-// });
-
-http.createServer(app).listen(port, host, () => {
-	console.log("Server ready at http://" + host + ":" + port);
+https.createServer({
+	key: fs.readFileSync('/var/cpanel/ssl/apache_tls/targethit.com/combined'),
+	cert: fs.readFileSync('/var/cpanel/ssl/apache_tls/targethit.com/combined'),
+	rejectUnauthorized:false
+},app).listen(port, host, () => {
+	console.log("Server ready at https://" + host + ":" + port);
 });
+
+// http.createServer(app).listen(port, host, () => {
+// 	console.log("Server ready at http://" + host + ":" + port);
+// });
